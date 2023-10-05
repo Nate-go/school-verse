97f40208f64349170ca20819e932f0a6b5ab01dc
Nate <nate.ha.goldenowl@gmail.com>
2023-10-05 08:39:01 +0700

    SCHOOL-33 deploy

---
+++ app/Constant/TableSetting.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Constant;
+
+class TableSetting
+{
+    const TEXT_TYPE = 0;
+
+    const IMG_TYPE = 1;
+
+    const TEXTARE_TYPE = 2;
+
+    const MIX_TYPE = 3;
+
+    const DECREASE_SORT = 'desc';
+
+    const INCREASE_SORT = 'asc';
+
+    const CONTAIN = 'contains';
+
+    const EQUAL = '=';
+
+    const GREATER = '>';
+
+    const SMALLER = '<';
+
+    const GREATER_EQUAL = '>=';
+
+    const SMALLER_EQUAL = '<=';
+
+    const BETWEEN_INCLUDE = '';
+
+    const BETWEEN_NOT_INCLUDE = 'bni';
+
+    const OUTSIDE_INCLUDE = 'osi';
+
+    const OUTSIDE_NOT_INCLUDE = 'osni';
+}
